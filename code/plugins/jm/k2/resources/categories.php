1dcf5b56ca9105fd2402306f162cffae862a4db5
Rafael <rxcorral@gmail.com>
2011-12-07 22:06:27 -0500

    Added ability to load category.
    Added more resources.
    Bug fixes.

--- code/plugins/jm/k2/resources/categories.php
+++ code/plugins/jm/k2/resources/categories.php
@@ -16,14 +16,135 @@ class K2JMResourceCategories extends JMResource
 {
 	public function get()
 	{
-		require_once JPATH_ADMINISTRATOR . '/components/com_k2/models/categories.php';
-
-		$categoriesModel = &JModel::getInstance( 'Categories', 'K2Model' );
+		jimport('joomla.html.parameter');
 
 		if ( JRequest::getVar('tree') ) {
+			JModel::addIncludePath( JPATH_ADMINISTRATOR . '/components/com_k2/models' );
+			$categoriesModel = &JModel::getInstance( 'Categories', 'K2Model' );
 			$response = $categoriesModel->categoriesTree(null, true, true);
+		} else {
+			if ( JRequest::getVar('all') ) {
+				$rows = $this->getCategories();
+			} else {
+				$rows = array();
+				foreach ( $this->getCategories() as $key => $_row ) {
+					$rows[] = (object) array(
+						'id' => $_row->id,
+						'title' => $_row->title,
+						'published' => $_row->published,
+						'access' => $_row->access,
+						'treename' => str_replace( '.&#160;&#160;&#160;&#160;&#160;&#160;',
+							'<span class="gi">|&mdash;</span>',
+							str_replace( '<sup>|_</sup>&#160;', '', $_row->treename ) )
+						);
+				}
+			}
+
+
+			$response = $rows;
 		}
 
 		$this->plugin->setResponse( $response );
+	}
+
+	/**
+	 * A copy of the getData method on this file:
+	 * /administrator/components/com_k2/models/categories.php
+	 * Some modifications have been made
+	 */
+	function getCategories()
+	{
+
+		$mainframe = &JFactory::getApplication();
+		$option = JRequest::getCmd('option');
+		$view = JRequest::getCmd('view');
+		$db = & JFactory::getDBO();
+		$limit = $mainframe->getUserStateFromRequest('global.list.limit', 'limit', $mainframe->getCfg('list_limit'), 'int');
+		$limitstart = $mainframe->getUserStateFromRequest($option.$view.'.limitstart', 'limitstart', 0, 'int');
+		$search = $mainframe->getUserStateFromRequest($option.$view.'search', 'search', '', 'string');
+		$search = JString::strtolower($search);
+		$filter_order = $mainframe->getUserStateFromRequest($option.$view.'filter_order', 'filter_order', 'c.ordering', 'cmd');
+		$filter_order_Dir = $mainframe->getUserStateFromRequest($option.$view.'filter_order_Dir', 'filter_order_Dir', '', 'word');
+		$filter_trash = $mainframe->getUserStateFromRequest($option.$view.'filter_trash', 'filter_trash', 0, 'int');
+		$filter_state = $mainframe->getUserStateFromRequest($option.$view.'filter_state', 'filter_state', -1, 'int');
+		$language = $mainframe->getUserStateFromRequest($option.$view.'language', 'language', '', 'string');
+
+		$query = "SELECT c.*, g.name AS groupname, exfg.name as extra_fields_group FROM #__k2_categories as c LEFT JOIN #__groups AS g ON g.id = c.access LEFT JOIN #__k2_extra_fields_groups AS exfg ON exfg.id = c.extraFieldsGroup WHERE c.id>0";
+
+		if (!$filter_trash){
+			$query .= " AND c.trash=0";
+		}
+
+		if ($search) {
+			$query .= " AND LOWER( c.name ) LIKE ".$db->Quote('%'.$db->getEscaped($search, true).'%', false);
+		}
+
+		if ($filter_state > -1) {
+			$query .= " AND c.published={$filter_state}";
+		}
+		if ($language) {
+			$query .= " AND c.language = ".$db->Quote($language);
+		}
+
+		$query .= " ORDER BY {$filter_order} {$filter_order_Dir}";
+
+		if(K2_JVERSION=='16'){
+			$query = JString::str_ireplace('#__groups', '#__viewlevels', $query);
+			$query = JString::str_ireplace('g.name AS groupname', 'g.title AS groupname', $query);
+		}
+
+		$db->setQuery($query);
+		$rows = $db->loadObjectList();
+		if(K2_JVERSION=='16'){
+			foreach($rows as $row){
+				$row->parent_id = $row->parent;
+				$row->title = $row->name;
+			}
+		}
+		$categories = array();
+
+		if ($search) {
+			foreach ($rows as $row) {
+				$row->treename = $row->name;
+				$categories[]=$row;
+			}
+
+		}
+		else {
+			$categories = $this->indentRows($rows);
+		}
+		if (isset($categories)){
+			$total = count($categories);
+		}
+		else {
+			$total = 0;
+		}
+		$categories = @array_slice($categories, $limitstart, $limit);
+		foreach($categories as $category) {
+			$category->parameters = new JParameter($category->params);
+			if($category->parameters->get('inheritFrom')) {
+				$db->setQuery("SELECT name FROM #__k2_categories WHERE id = ".(int)$category->parameters->get('inheritFrom'));
+				$category->inheritFrom = $db->loadResult();
+			}
+			else {
+				$category->inheritFrom = '';
+			}
+		}
+		return $categories;
+	}
+
+	function indentRows( & $rows) {
+		$children = array ();
+		if(count($rows)){
+			foreach ($rows as $v) {
+				$pt = $v->parent;
+				$list = @$children[$pt]?$children[$pt]: array ();
+				array_push($list, $v);
+				$children[$pt] = $list;
+			}
+		}
+				
+		$categories = JHTML::_('menu.treerecurse', 0, '', array (), $children);
+		return $categories;
 	}
 }
--- code/plugins/jm/k2/resources/formfields.php
+++ code/plugins/jm/k2/resources/formfields.php
@@ -12,7 +12,7 @@ defined('_JEXEC') or die;
 
 jimport('joomla.plugin.plugin');
 
-class K2JMResourceItemForm extends JMResource
+class K2JMResourceFormFields extends JMResource
 {
 	public function get()
 	{
@@ -21,7 +21,7 @@ class K2JMResourceItemForm extends JMResource
 		JFactory::getLanguage()->load( 'com_k2', JPATH_ADMINISTRATOR );
 
 		$form = JForm::getInstance( 'itemForm', JPATH_ADMINISTRATOR
-			. '/components/com_k2/models/item.xml' );
+			. '/components/com_k2/models/' . JRequest::getWord('form') . '.xml' );
 
 		$html = '';
 		foreach ( $form->getFieldset( JRequest::getVar('fieldset') ) as $field ) {
@@ -29,8 +29,10 @@ class K2JMResourceItemForm extends JMResource
 			if( in_array( $field->type, array( 'header', 'Spacer' ) ) ){
 				continue;
 			} else {
+				// Lets make sure the for="" on the label matches the id ont he input
+				preg_match( '/id="(.*?)"/', $field->input, $matches );
 				$html .= '<div data-role="fieldcontain">'
-					. $field->label
+					. preg_replace( '/for="(.*?)"/', 'for="' .$matches[1]. '"', $field->label )
 					. $field->input
 					. '</div>';
 			}
